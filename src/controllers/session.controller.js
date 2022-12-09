a5ba4045b37199c7ae910a7fd19f04dd19f05178
tricefrey <trice.frey@outlook.com>
2022-12-10 01:08:02 +0700

    select query

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -9,7 +9,7 @@ const findById = 'SELECT * FROM test_session WHERE id = $1'
 const findByCategoryId = 'SELECT * FROM test_session WHERE category_id = $1'
 const findByStatus = 'SELECT * FROM test_session WHERE status = $1'
 const insert = 'INSERT INTO test_session (category_id, description, pre_test_msg, time_limit, status, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7)'
-const upadate = 'UPDATE test_session SET category_id = $1, description = $2,  pre_test_msg = $3, time_limit = $4, updated_at = $5 WHERE id = $6'
+const upadate = 'UPDATE test_session SET category_id = $1, description = $2,  pre_test_msg = $3, time_limit = $4, updated_at = $5, status = $6 WHERE id = $7'
 const upadateStatus = 'UPDATE test_session SET status = $1, updated_at = $2 WHERE id = $3'
 
 exports.insert = (req, res) => {
@@ -67,10 +67,10 @@ exports.getById = (req, res) => {
 
 exports.update = (req, res) => {
   const id = req.params.id
-  const { category_id, description,  pre_test_msg, time_limit } = req.body
+  const { category_id, description,  pre_test_msg, time_limit, status} = req.body
   const date = new Date()
 
-  db.query(upadate, [category_id, description,  pre_test_msg, time_limit, date, id], (error, results) => {
+  db.query(upadate, [category_id, description,  pre_test_msg, time_limit, date, status, id], (error, results) => {
     if (error) {
       response.error500(res, error.message)
     } else {
@@ -81,13 +81,13 @@ exports.update = (req, res) => {
 
 exports.delete = (req, res) => {
   const id = req.params.id
-  const { status } = req.body
   const date = new Date()
+  const status = 0
   
   db.query(upadateStatus, [status, date, id], (error, results) => {
     if (error) {
       response.error500(res, error.message)
     } 
-    response.build(res, 201, true, `Session status updated`, null, null)
+    response.build(res, 201, true, `Session was deleted successfully`, null, null)
   })
 }
