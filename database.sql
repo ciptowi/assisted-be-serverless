219c88684816a903af8a37d266583a6d99014fb8
tricefrey <trice.frey@outlook.com>
2022-12-09 21:49:34 +0700

    create alter

--- database.sql
+++ database.sql
@@ -22,12 +22,8 @@ CREATE TABLE question (
   status INT NOT NULL,
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
-  FOREIGN KEY (admin_id) REFERENCES admin (id),
   FOREIGN KEY (category_id) REFERENCES category (id)
 );
-
-COPY question(id, admin_id, category_id, content, status, created_at, updated_at)
-FROM 'C:\Users\cipto\Desktop\question.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE answer (
   id serial PRIMARY KEY,
@@ -42,23 +38,22 @@ CREATE TABLE answer (
 
 CREATE TABLE test_session (
   id serial PRIMARY KEY,
-  category_id INT NOT NULL,
+  category_id VARCHAR (255),
   description VARCHAR (max),
   pre_test_msg VARCHAR (max),
   time_limit INT NOT NULL,
   status INT NOT NULL,
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
-  FOREIGN KEY (category_id) REFERENCES category (id)
 );
 
-CREATE TABLE partisipant (
+CREATE TABLE participant (
   id serial PRIMARY KEY,
   category_id INT NOT NULL,
   test_session_id INT NOT NULL,
-  nik VARCHAR (50),
+  nik VARCHAR (255),
   name VARCHAR (255),
-  partisipant_numb VARCHAR (50),
+  participant_numb VARCHAR (255),
   score INT NOT NULL,
   status INT NOT NULL,
   created_at TIMESTAMP,
@@ -69,11 +64,14 @@ CREATE TABLE partisipant (
 
 CREATE TABLE test_result (
   id serial PRIMARY KEY,
-  partisipant_id INT NOT NULL,
+  participant_id INT NOT NULL,
   answer_id INT NOT NULL,
   status INT NOT NULL,
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
-  FOREIGN KEY (partisipant_id) REFERENCES partisipant (id),
+  FOREIGN KEY (participant_id) REFERENCES participant (id),
   FOREIGN KEY (answer_id) REFERENCES answer (id)
 );
+
+COPY question(id, admin_id, category_id, content, status, created_at, updated_at)
+FROM 'C:\Users\cipto\Desktop\question.csv' DELIMITER ',' CSV HEADER;
