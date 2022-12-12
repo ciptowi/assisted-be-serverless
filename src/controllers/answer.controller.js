40d3d2e6cdf2ec687738bdeb4242e5e507ad4321
tricefrey <trice.frey@outlook.com>
2022-12-13 04:31:03 +0700

    get answer join question by category_id

--- src/controllers/answer.controller.js
+++ src/controllers/answer.controller.js
@@ -53,7 +53,7 @@ exports.get = (req, res) => {
       }
       response.success(res, results.rows)
     })
-  } else if (status == 0 && question_id == 0){
+  } else if (status == 0 && question_id == 0 && category_id == 0){
     db.query(findAll, (error, results) => {
       if (error) {
         response.error500(res, error.message)
